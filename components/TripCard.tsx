0a29e0cd8bd5c337c5caff260cf2d0f0eee32ea0
Miles Wildmore <mileswildmore18@gmail.com>
2025-04-29 14:31:06 -0400

    Added the captions for each location featured and where they are located.

--- components/TripCard.tsx
+++ components/TripCard.tsx
@@ -1,15 +1,31 @@
 // Import the components
 import {Link, useLocation} from "react-router";
-// Tripcard with destructuring
-const TripCard = ({ id, name, location, imageUrl, tags, price }:TripCardProps) => {
+// Trip card with destructuring
+const TripCard = ({id, name, location, imageUrl, tags, price}: TripCardProps) => {
     // Destructure the location and path
     const path = useLocation();
 
     return (
         // Stylize the trip card and link to locations based on id
-        <Link to={path.pathname === "/" || path.pathname.startsWith('/travel') ? `/travel/${id}` : `/trips/${id}`}>
+        <Link to={path.pathname === "/" || path.pathname.startsWith('/travel') ? `/travel/${id}` : `/trips/${id}`}
+              className="trip-card">
             {/* Add the image*/}
             <img src={imageUrl} alt={name}/>
+
+            {/* Add the location*/}
+            <article>
+                {/* Add the location name*/}
+                <h2>{name}</h2>
+                {/*Add the location mark picture*/}
+                <figure>
+                    <img
+                        src="/assets/icons/location-mark.svg"
+                        alt="location"
+                        className="size-4"/>
+                    {/* Add the country for the location*/}
+                    <figcaption>{location}</figcaption>
+                </figure>
+            </article>
         </Link>
     )
 }
